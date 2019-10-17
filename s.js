38cd3aeeac337bd673665b92b4da40329d3a25a0
shreyas <shreyasls@yahoo.com>
2019-10-17 00:59:09 -0400

    Better handling of promises. Also generating video thumbnails

--- s.js
+++ s.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Database = require('better-sqlite3');
 const db = new Database('meta.db', {  }); //verbose: console.log
 const exiftool = require("exiftool-vendored").exiftool;
+const ffmpeg = require('fluent-ffmpeg');
 
 const util = require('util');
 
@@ -34,7 +35,7 @@ let albums= {
   2011: "/media/windows/PHOTOS/2011/"
 }
 
-const album = "TEST";
+const album = "PHOTOS";
 
 function getFilesMtime(files){
   return new Promise((resolve,reject)=>{
@@ -97,8 +98,56 @@ const insertMany = db.transaction((records)=>{
   }
 });
 
-Promise.all([filesPromise, metaPromise])
-.then(function(values){
+const rotationMapping = {
+  0: 1,
+  90: 6,
+  180: 3,
+  270: 8
+};
+
+function extractVideoThumbnail(videoTags){
+
+  var tags=videoTags;
+
+  return new Promise((resolve,reject)=>{
+    let sourceFileName = tags.Directory+'/'+tags.FileName,
+      videoThumbnailFileName = album.concat('/', tags.Directory.replace(albums[album], ""), '/', tags.FileName, '.jpg').replace(/\//gi, '_');
+
+    // extract thumbnail
+    ffmpeg(sourceFileName)
+      .on("error", function(error){
+        console.log(error)
+      })
+      .on("start", function(cmd){
+       console.log("Running: "+cmd)
+      })
+      .thumbnail({
+        count: 1,
+        folder: '.video-thumbnails',
+        filename: videoThumbnailFileName,
+        size: tags.ImageSize||"1040x640"
+      })
+      .on("end", async function(){
+        console.log("writing tags...");
+
+        try{
+          var writeTag = await exiftool.write('.video-thumbnails/'+videoThumbnailFileName, {
+            "Orientation#": rotationMapping[tags.Rotation]
+          });
+          resolve('.video-thumbnails/'+videoThumbnailFileName);
+        } catch(err) {
+          console.log("EXIFTOOL ERROR during Orientation "+ err)
+          reject(err);
+        }
+      });
+  });
+}
+
+async function run(){
+  var values = await Promise.all([filesPromise, metaPromise]);
+
+// Promise.all([filesPromise, metaPromise])
+// .then(function(values){
   let fs=values[0], meta=values[1], delta=[], removed=[];
   log("fs "+Object.keys(fs).length + " meta "+Object.keys(meta).length);
   
@@ -129,7 +178,7 @@ Promise.all([filesPromise, metaPromise])
     }))
 
 
-    log("Starting Promises");
+    log("Starting Promises to extract metadata using exiftool");
     
     var exifPromises = [];
 
@@ -138,69 +187,95 @@ Promise.all([filesPromise, metaPromise])
       exifPromises.push(p);
     });
 
-    Promise.all(exifPromises)
-    .then(promiseReturns=>{
+    var exifMetaData = await Promise.all(exifPromises);
+//     .then(exifMetaData=>{
 
-      log("All Promises Completed; Starting DELETE");
+    log("All Promises Completed; Starting DELETE");
 
-      // first delete the record (if any)
-      deleteMany(promiseReturns.map(tags=>{
-        return { 
-          album: album,
-          filename: tags.Directory.replace(albums[album], "")+"/"+tags.FileName
-        }
-      }));
+    // first delete the record (if any)
+    deleteMany(exifMetaData.map(tags=>{
+      return { 
+        album: album,
+        filename: tags.Directory.replace(albums[album], "")+"/"+tags.FileName
+      }
+    }));
 
-      log("Starting INSERT");
+    // extract video thumbnails
+    let videosExif = exifMetaData.filter(tags=>{
+      //log(util.inspect(tags));
+      let mimetype = tags["MIMEType"]||"x"
+      return mimetype.startsWith("video")
+    });
 
-      // now, insert the new metadata
-      insertMany(promiseReturns.map(tags=>{
+    log("# of Videos: "+videosExif.length);
 
-        let imageSize = tags.ImageSize ? tags.ImageSize.split(/x/i) : "", 
-          aspectRatio = imageSize ? 
-            tags.orientation ? 
-              [6,8].indexOf(tags.orientation) ? imageSize[1]/imageSize[0] : imageSize[0]/imageSize[1] 
-            : imageSize[0]/imageSize[1] 
-          : 0;
+    var videoThumbPromises = [];
+    videosExif.forEach(video=>{
+      videoThumbPromises.push( extractVideoThumbnail(video) );
+    });
 
-        return {
-          album: album,
-          filename: tags.Directory.replace(albums[album], "")+"/"+tags.FileName,
-          folder: tags.Directory.replace(albums[album], ""),
-          filesize: tags.FileSize||null,
-          ext: tags.FileName.split(".").pop(),
-          mimetype: tags.MIMEType||null,
-          keywords: tags.Keywords ? ((typeof(tags.Keywords) == "string") ?  tags.Keywords : tags.Keywords.join(", ")) : null,
-          faces: tags.RegionInfo ? tags.RegionInfo.RegionList.map(d=>d.Name).join(", ") : null,
-          rating: tags.Rating||null,
-          imagesize: tags.ImageSize||null,
-          aspectratio: aspectRatio,
-          make: tags.Make||null,
-          model: tags.Model||null,
-          orientation: tags.Orientation||null,
-          datetimeoriginal: tags.DateTimeOriginal ? tags.DateTimeOriginal.toString() : null,
-          createdate: tags.CreateDate ? tags.CreateDate.toString() : null,
-          filemodifydate: tags.FileModifyDate ? tags.FileModifyDate.toString() : null,
-          filedate: tags.DateTimeOriginal ? tags.DateTimeOriginal.toString() : (tags.CreateDate ? tags.CreateDate.toString() : (tags.FileModifyDate.toString() ))
-        }
-      }));
+    log("Starting extraction of video Thumbnails");
 
-    })
-    .catch(err=>{
-      log("PROMISE ERROR ${err}");
-     })
-    .finally(()=>{
-      exiftool.end();
-      log("In FINALLY block");
+    await Promise.all(videoThumbPromises);
 
-     });
+    log("Starting INSERT");
+
+    // now, insert the new metadata
+    insertMany(exifMetaData.map(tags=>{
+
+      let imageSize = tags.ImageSize ? tags.ImageSize.split(/x/i) : "";
+
+      // TODO: Different for video needed here
+      let
+        aspectRatio = imageSize ? 
+          tags.Orientation ? 
+            ([6,8].indexOf(tags.Orientation) >= 0) ? imageSize[1]/imageSize[0] : imageSize[0]/imageSize[1] 
+          : imageSize[0]/imageSize[1] 
+        : 0;
+
+      return {
+        album: album,
+        filename: tags.Directory.replace(albums[album], "")+"/"+tags.FileName,
+        folder: tags.Directory.replace(albums[album], ""),
+        filesize: tags.FileSize||null,
+        ext: tags.FileName.split(".").pop().toLowerCase(),
+        mimetype: tags.MIMEType||null,
+        keywords: tags.Keywords ? ((typeof(tags.Keywords) == "string") ?  tags.Keywords : tags.Keywords.join(", ")) : null,
+        faces: tags.RegionInfo ? tags.RegionInfo.RegionList.map(d=>d.Name).join(", ") : null,
+        rating: tags.Rating||null,
+        imagesize: tags.ImageSize||null,
+        aspectratio: aspectRatio,
+        make: tags.Make||null,
+        model: tags.Model||null,
+        orientation: tags.Orientation||null,
+        datetimeoriginal: tags.DateTimeOriginal ? tags.DateTimeOriginal.toString() : null,
+        createdate: tags.CreateDate ? tags.CreateDate.toString() : null,
+        filemodifydate: tags.FileModifyDate ? tags.FileModifyDate.toString() : null,
+        filedate: tags.DateTimeOriginal ? tags.DateTimeOriginal.toString() : (tags.CreateDate ? tags.CreateDate.toString() : (tags.FileModifyDate.toString() ))
+      }
+    }));
+
+//     })
+//     .catch(err=>{
+//       log(`PROMISE ERROR ${err}`);
+//      })
+//     .finally(()=>{
+//       exiftool.end();
+//       log("In FINALLY block");
+
+//      });
   } catch(err){
     console.error("ERROR: ", err)
+  } finally {
+    log("In FINALLY block");
+    exiftool.end();
   }
   
-});
+}
 
 function log(str){
   var d = new Date(); 
   console.log(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} --> ${str}`);
 }
+
+run();
